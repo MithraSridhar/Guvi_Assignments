82314370709572a128378af2dd29507a5713e6c3
mithrasridhar <mithugopal30@gmail.com>
2023-09-25 17:52:14 +0530

    Assignment 2 update

--- Day3-250923/program3.js
+++ Day3-250923/program3.js
@@ -2,7 +2,7 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var jObj=new XMLHttpRequest();
 jObj.open("Get","https://restcountries.com/v3.1/all",false)
-jObj.send(null)
+jObj.send()
 var text = jObj.responseText;
 var parsedJson=JSON.parse(text)
 var jsonLength = parsedJson.length
